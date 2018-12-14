9ac9e4e1c6ffa6c04088a450b02d00c0ffe7a075
kilaws444 <kilaws44@mail.com>
2018-12-14 10:47:23 -0300

    Initial commit

---
+++ src/environments/firebaseConfig.ts
@@ -0,0 +1,8 @@
+export const FireBaseConfig = {
+  apiKey: "AIzaSyBCMo2wv0xvfrN68QzVM8K1ummTDLDXH2g",
+  authDomain: "angular-shoppingcart-57caa.firebaseapp.com",
+  databaseURL: "https://angular-shoppingcart-57caa.firebaseio.com",
+  projectId: "angular-shoppingcart-57caa",
+  storageBucket: "angular-shoppingcart-57caa.appspot.com",
+  messagingSenderId: "985927680263"
+};
